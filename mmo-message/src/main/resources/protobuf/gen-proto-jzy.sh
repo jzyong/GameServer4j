345b1e482f50f2f5576d89bc7f34e96b21d4d428
jzyong <359135103@qq.com>
2020-02-01 20:07:00 +0800

    go protobuf

--- mmo-message/src/main/resources/protobuf/gen-proto-jzy.sh
+++ mmo-message/src/main/resources/protobuf/gen-proto-jzy.sh
@@ -1,9 +1,6 @@
-
-
-
-
-
-
-protoc --java_out /Users/jiangzhiyong/Project/game/server/mmo-server/mmo-message/src/main/java ./MIDMessage.proto
-
-
+#java
+protoc --java_out /Users/jiangzhiyong/Project/game/server/mmo-server/mmo-message/src/main/java ./*.proto
+#go
+protoc --go_out=/Users/jiangzhiyong/Project/go/src/github.com/jzy/go-mmo-server/src/message *.proto
+#copy proto to go project
+cp *.proto /Users/jiangzhiyong/Project/go/src/github.com/jzy/go-mmo-server/src/message/proto
