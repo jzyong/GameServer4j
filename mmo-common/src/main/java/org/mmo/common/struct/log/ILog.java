4a3a2bde0eb9646878b242408ade1292491dff29
jzyong <jzy072535>
2021-02-08 17:16:13 +0800

    log

---
+++ mmo-common/src/main/java/org/mmo/common/struct/log/ILog.java
@@ -0,0 +1,23 @@
+package org.mmo.common.struct.log;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * @author jzy
+ * @mail 359135103@qq.com
+ */
+public interface ILog {
+
+    long getId();
+
+    String topic();
+
+    default String key() {
+        return String.valueOf(getId());
+    }
+
+    default String value() {
+        return JSON.toJSONString(this);
+    }
+
+}
